2d70b8a5c6151d60742541ceb272cbb238c664c0
Igor Šula <sulaigor@gmail.com>
2019-08-11 01:09:25 +0200

    Adds users seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+users_seed = [
+  {
+    name: "Applifter",
+    email: "info@applifting.cz",
+    access_token: "93f39e2f-80de-4033-99ee-249d92736a25"
+  },
+  {
+    name: "Batman",
+    email: "batman@applifting.cz",
+    access_token: "dcb20f8a-5657-4f1b-9f7f-ce65739b359e"
+  }
+]
+
+users_seed.each do |user|
+  User.create!(user)
+end
